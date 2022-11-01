99b0f84d89b659ee3f2cfabcc22a3e4cbcc907ab
ThePixelScript <101989062+ThePixelScript@users.noreply.github.com>
2022-11-01 09:10:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var up = document.getElementById('up');
 var right = document.getElementById('right');
 var down = document.getElementById('down');
 
-left.addEventListener('click',vibrate(30))
-up.addEventListener('click',vibrate(100))
-right.addEventListener('click',vibrate(300))
-down.addEventListener('click',vibrate(700))
+left.addEventListener('click',function(){vibrate(30)})
+up.addEventListener('click',function(){vibrate(100)})
+right.addEventListener('click',function(){vibrate(300)})
+down.addEventListener('click',function(){vibrate(700)})
 
 function vibrate(ms)
 {
-    navigator.vibrate(ms);
+  console.log("clicked")
+    window.navigator.vibrate(ms);
+    
 }
