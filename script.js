44cf19b04f103a7643dc4b49bac997e0c9919f98
ThePixelScript <101989062+ThePixelScript@users.noreply.github.com>
2022-11-01 10:28:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,35 @@ var left = document.getElementById('left');
 var up = document.getElementById('up');
 var right = document.getElementById('right');
 var down = document.getElementById('down');
-
-left.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
-up.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
-right.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
-down.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
+var inSpace = 0
+var progress = 0
+left.addEventListener('click',function(){vibrate(randomInteger(500,800,0))})
+up.addEventListener('click',function(){vibrate(randomInteger(500,800,1))})
+right.addEventListener('click',function(){vibrate(randomInteger(500,800,2))})
+down.addEventListener('click',function(){vibrate(randomInteger(500,800,3))})
 
 function vibrate(ms)
 {
-  console.log("clicked")
+ 
     window.navigator.vibrate(ms);
     
 }
 
-function randomInteger(min, max) {
+function randomInteger(min, max,space) {
     var rand = Math.floor(Math.random() * (max - min + 1)) + min;
-    if (rand < 600)
+    if (space == inSpace)
     {
-return 50
+        nextSpace()
+        progress++
+    return 50
     }
     else
     {
     return rand
     }
+  }
+
+  function nextSpace() {
+    var nextSpace = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+    inSpace = nextSpace;
   }
