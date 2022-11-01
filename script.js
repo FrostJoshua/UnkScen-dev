9813a509125ed1ec9a600dba59f223e95d0abeee
ThePixelScript <101989062+ThePixelScript@users.noreply.github.com>
2022-11-01 09:55:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var up = document.getElementById('up');
 var right = document.getElementById('right');
 var down = document.getElementById('down');
 
-left.addEventListener('click',function(){vibrate(30)})
-up.addEventListener('click',function(){vibrate(100)})
-right.addEventListener('click',function(){vibrate(300)})
-down.addEventListener('click',function(){vibrate(700)})
+left.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
+up.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
+right.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
+down.addEventListener('click',function(){vibrate(randomInteger(500,1000))})
 
 function vibrate(ms)
 {
@@ -14,3 +14,15 @@ function vibrate(ms)
     window.navigator.vibrate(ms);
     
 }
+
+function randomInteger(min, max) {
+    var rand = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (rand < 600)
+    {
+return 50
+    }
+    else
+    {
+    return rand
+    }
+  }
